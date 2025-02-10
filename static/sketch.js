4d7a5fdc9512d47c3ffe0fb8266ef8db0f2eb1f0
LanceryH <lancery.hugo@outlook.com>
2025-02-10 23:25:41 +0000

    WOOWW

--- static/sketch.js
+++ static/sketch.js
@@ -1,45 +1,27 @@
-let socket;
-let names = []; 
-let sat_data;
-let orbit_status = 0;
-let selected_status;
-
 function preload() {
-    // img = loadImage(imagePath); // Use the image path passed from HTML
+    img = loadImage(imagePath); // Use the image path passed from HTML
 }
 
 function setup() {
-    cnv = createCanvas(windowWidth, windowHeight, WEBGL); // Create a drawing canvas
-    cnv.position(0, 0, "fixed");
-    angleMode(DEGREES);
-    socket = io(); 
+    let width = document.getElementById('sketch').clientWidth;
+    let height = document.getElementById('sketch').clientHeight;
 
-    socket.on('send_data', function(data) {
-        names = data["Names"];
-        colors = data["Colors"];
-        sat_data = data;
-    });
+    const canvas = createCanvas(width, height, WEBGL);
+    canvas.parent('sketch');
+    background(220);
 }
 
 function draw() {
-    console.log(orbit_status)
-    resizeCanvas(windowWidth, windowHeight)
-
-    clear();
     background(0);
     orbitControl(1, 1, 1);
-    scale(0.02);
-    perspective(180 / 3.0, width / height, 0.1, 15000000);
+    scale(0.03);
 
     push();
-    // texture(img);
-    strokeWeight(1);
-    noFill();
+    texture(img);
+    strokeWeight(0);
     rotateX(0);
     rotateY(-180);
     rotateZ(0);
-    stroke(255,0,0)
     sphere(6371, 24, 24);
-    pop();  
-   
+    pop();
 }
